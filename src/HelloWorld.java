e2bbc1c8fff164b49cad25efc356adc7992e239d
gate2sandesh <63482895+gate2sandesh@users.noreply.github.com>
2021-02-18 14:27:00 -0600

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Nice day for fishing aint it");
+    }
+}
---
+++ src/com/hello2021/company/Sandesh.java
@@ -0,0 +1,11 @@
+package com.hello2021.company;
+
+public class Sandesh {
+    public static void main(String[] args) {
+
+        System.out.println("Hello");
+        System.out.println("Happy New Year");
+        System.out.println("Jan 7th");
+    }
+
+}
